3eda82487d2969d5fc1becccf9694c773d5af036
Joe Tannenbaum <joe@joe.codes>
2015-07-23 15:42:02 -0400

    added datetime directive

---
+++ src/Directive/Datetime.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Templater\Directive;
+
+class Listize extends Directive {
+
+    protected function getName()
+    {
+        return 'list';
+    }
+
+    protected function directive($expression)
+    {
+        return "<?php echo Carbon\Carbon::parse{$expression}->format('F j, Y g:ia T'); ?>";
+    }
+
+}
